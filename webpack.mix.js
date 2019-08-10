adc4ee4d36b9fec904a62f08a2c30b5078b50617
Nick Poulos <nickpoulos.ny@gmail.com>
2019-08-10 00:34:43 -0400

    Update webpack.mix.js
    Add doc/video copyDiretory calls

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,6 @@ const mix = require('laravel-mix');
 
  mix.js('resources/js/app.js', 'public/js')
     //.sass('resources/sass/app.scss', 'public/css')
-    .copyDirectory('resources/img', 'public/img');
+    .copyDirectory('resources/doc', 'public/doc')
+    .copyDirectory('resources/img', 'public/img')
+    .copyDirectory('resources/video', 'public/video');
